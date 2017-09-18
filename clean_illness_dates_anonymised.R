c79ac967c0e57c5e94c562425384f5321ea6d47c
CharlotteWoolley <s1576473@sms.ed.ac.uk>
2017-09-18 14:21:33 +0100

    Finished data generation
    Added code to make duplicates in the example data and tidied it up a
    bit.
    Also started writing anonymised code for cleaning

--- clean_illness_dates_anonymised.R
+++ clean_illness_dates_anonymised.R
@@ -1,5 +1,38 @@
-#This code was originally written in order to clean owner reported start and end 
+#FILENAME: clean_illness_dates_anonymised.R
+#AUTHOR: Charlotte Woolley
+#DESCRIPTION: This code was originally written in order to clean owner reported start and end 
 #dates for illnesses in dogs enrolled in the Dogslife project
-#it has been anonymised and adapted to be used on an example dataset
+#it has been anonymised and adapted to be used on an example dataset 
+#PACKAGES NEEDED:
+        library(plyr)
+        library(dplyr)
+        library(lubridate)
+        library(data.table)
 
+#read in data and format 
+
+        dat <- read.csv('example_dates_data.csv')
+        dat2 <- dat[,2:7] 
+        dat2 <- dat2 %>%
+                mutate(start_date_new = start_date,
+                       end_date_new = end_date,
+                       visit_date_new = visit_date)
+        
+#create columns to identify duplications in the ID and to identify whether the 
+#entire data entry has been completely duplicated
+        
+        dat2 <- dat2 %>%
+                mutate(duplications = (duplicated(dups_ID) | 
+                                               duplicated(dups_ID, 
+                                                          fromLast = TRUE)),
+                       complete_dups_ID = group_indices_(dat2, 
+                                .dots=c("ID", "start_date_new", 
+                                        "end_date_new", "visit_date_new")),
+                       complete_duplications = (duplicated(complete_dups_ID) | 
+                                                duplicated(complete_dups_ID, 
+                                                           fromLast = TRUE)))
+        
+        
+        sum(dat2$duplications) #2392 duplications
+        sum(dat2$complete_duplications) #808 complete duplications
 
--- example_data_generation.R
+++ example_data_generation.R
@@ -3,24 +3,19 @@
 #DESCRIPTION: This code documents how an example dataset was generated for 
 #testing a data cleaning method for dates in illness records
 #PACKAGES NEEDED:
-        library(plyr)
         library(dplyr)
 
-#Make an ID variable for each simulated individual
-        ID <- as.factor(1:10000)
-        
 #Set the start of time and end of time for the data period generated        
         st <- '2000/01/01'
         et <- '2010/01/01'
-        
-#Make a randomly generated date of birth variable for each simulated individual,
-#that assumes all individuals were born between within the start and end of time
 
+#Make a dataframe with an ID variable for each simulated individual and a randomly 
+#generated date of birth variable for each simulated individual, that assumes all
+#individuals were born between within the start and end of time
         set.seed(777)
-        date_of_birth <- sample(seq(as.Date(st), as.Date(et), 
-                                    by="day"), 10000, replace = TRUE)
-#Make a dataframe 
-        dat <- data.frame(ID, date_of_birth)
+        dat <- tibble(ID = as.factor(1:10000), 
+                      date_of_birth = sample(seq(as.Date(st), 
+                                 as.Date(et), by="day"), 10000, replace = TRUE))
 
 #Function that generates random dates for when the incidents were recorded and 
 #for start, end and visit dates
@@ -50,44 +45,62 @@
         
 #This applies the function to each row, with a minimum difference between
 #the date of birth and date recorded set at 28 days
-        dat <- ddply(dat, 1, get_dates, min_diff = 28)
+
+        dat <- dat %>%
+                dplyr::group_by(ID) %>%
+                dplyr::do(get_dates(., min_diff = 28)) %>%
+                dplyr::ungroup()
+        
+#randomly select data to duplicate
+
+        r <- 0.07 #percentage of rows to duplicate once
+        s <- 0.03 #percentage of rows to dupicate twice
+        
+        set.seed(782)
+        sub <- dat[sample(nrow(dat), (length(dat$ID)*r)), ]
+        set.seed(783)
+        subb <- dat[sample(nrow(dat), (length(dat$ID)*s)), ]
+        sub_expand1 <- sub[rep(1:nrow(sub),each=2),] 
+        sub_expand2 <- subb[rep(1:nrow(subb),each=3),] 
+        dat <- join(dat,sub_expand1)
+        dat <- join(dat,sub_expand2)
         
 #randomly select data to add errors to
 
         p <- 0.1 #percentage of data that may contain errors
         
         #start dates
-        set.seed(782)
-        sub1 <- dat[sample(nrow(dat), (max(dat$ID)*p)), ]
+        set.seed(784)
+        sub1 <- dat[sample(nrow(dat), (length(dat$ID)*p)), ]
         sub1$SD_error_gen <- TRUE
         dat <- join(dat,sub1)
-        set.seed(783)
+        set.seed(785)
         dat$random_SDs <- sample(seq(as.Date(st), as.Date(et), 
-                                     by="day"), 10000, replace = TRUE)
+                                     by="day"), length(dat$ID), replace = TRUE)
         dat$start_date <- ifelse(dat$SD_error_gen == TRUE & !is.na(dat$SD_error_gen), 
                                   dat$random_SDs, dat$start_date)
         dat$start_date <- as.Date(dat$start_date, origin="1970-01-01")
 
         #end dates
-        set.seed(784)
-        sub2 <- dat[sample(nrow(dat), (max(dat$ID)*p)), ]
+        set.seed(786)
+        sub2 <- dat[sample(nrow(dat), (length(dat$ID)*p)), ]
         sub2$ED_error_gen <- TRUE
         dat <- join(dat,sub2)
-        set.seed(785)
+        set.seed(787)
         dat$random_EDs <- sample(seq(as.Date(st), as.Date(et), 
-                                     by="day"), 10000, replace = TRUE)
+                                     by="day"), length(dat$ID), replace = TRUE)
         dat$end_date <- ifelse(dat$ED_error_gen == TRUE & !is.na(dat$ED_error_gen), 
                                dat$random_EDs, dat$end_date)
         dat$end_date <- as.Date(dat$end_date, origin="1970-01-01")
         
         #visited vet dates
-        set.seed(786)
-        sub3 <- dat[sample(nrow(dat), (max(dat$ID)*p)), ]
+        set.seed(788)
+        sub3 <- dat[sample(nrow(dat), (length(dat$ID)*p)), ]
         sub3$VD_error_gen <- TRUE
         dat <- join(dat,sub3)
-        set.seed(787)
+        set.seed(789)
         dat$random_VDs <- sample(seq(as.Date(st), as.Date(et), 
-                                     by="day"), 10000, replace = TRUE)
+                                     by="day"), length(dat$ID), replace = TRUE)
         dat$visit_date <- ifelse(dat$VD_error_gen == TRUE & !is.na(dat$VD_error_gen),
                                  dat$random_VDs, dat$visit_date)
         dat$visit_date <- as.Date(dat$visit_date, origin="1970-01-01")
@@ -95,35 +108,35 @@
         
 #randomly select data to add missing data to
         
-        q <- 0.05 #percentage of data that contains missing data       
+        q <- 0.1 #percentage of data that contains missing data       
         
         #start dates
-        set.seed(788)
-        sub4 <- dat[sample(nrow(dat), (max(dat$ID)*q)), ]
-        sub4$SD_error_gen2 <- TRUE
+        set.seed(790)
+        sub4 <- dat[sample(nrow(dat), (length(dat$ID)*q)), ]
+        sub4$SD_NAs <- TRUE
         dat <- join(dat,sub4)
-        set.seed(789)
-        dat$start_date <- ifelse(dat$SD_error_gen2 == TRUE & !is.na(dat$SD_error_gen2), 
+        set.seed(791)
+        dat$start_date <- ifelse(dat$SD_NAs == TRUE & !is.na(dat$SD_NAs), 
                                  NA, dat$start_date)
         dat$start_date <- as.Date(dat$start_date, origin="1970-01-01")
         
         #end dates
-        set.seed(790)
-        sub5 <- dat[sample(nrow(dat), (max(dat$ID)*q)), ]
-        sub5$ED_error_gen2 <- TRUE
+        set.seed(792)
+        sub5 <- dat[sample(nrow(dat), (length(dat$ID)*q)), ]
+        sub5$ED_NAs <- TRUE
         dat <- join(dat,sub5)
-        set.seed(791)
-        dat$end_date <- ifelse(dat$ED_error_gen2 == TRUE & !is.na(dat$ED_error_gen2), 
+        set.seed(793)
+        dat$end_date <- ifelse(dat$ED_NAs == TRUE & !is.na(dat$ED_NAs), 
                                NA, dat$end_date)
         dat$end_date <- as.Date(dat$end_date, origin="1970-01-01")
         
         #visited vet dates
-        set.seed(792)
-        sub6 <- dat[sample(nrow(dat), (max(dat$ID)*q)), ]
-        sub6$VD_error_gen2 <- TRUE
+        set.seed(794)
+        sub6 <- dat[sample(nrow(dat), (length(dat$ID)*q)), ]
+        sub6$VD_NAs <- TRUE
         dat <- join(dat,sub6)
-        set.seed(793)
-        dat$visit_date <- ifelse(dat$VD_error_gen2 == TRUE & !is.na(dat$VD_error_gen2),
+        set.seed(795)
+        dat$visit_date <- ifelse(dat$VD_NAs == TRUE & !is.na(dat$VD_NAs),
                                  NA, dat$visit_date)
         dat$visit_date <- as.Date(dat$visit_date, origin="1970-01-01")
         
